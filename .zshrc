cc25c16cfe39cdaf63edbde58fa25d75abe65875
Artem Sharipov <sharipovartem@github.com>
2025-09-07 15:06:48 -0400

    My dotfiles

---
+++ .zshrc
@@ -0,0 +1,46 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+
+# Path to your oh-my-zsh installation.
+export ZSH="$HOME/.oh-my-zsh"
+
+# Set name of the theme to load. Look in ~/.oh-my-zsh/themes/
+ZSH_THEME="powerlevel10k/powerlevel10k"
+# Which plugins would you like to load?
+# Standard plugins can be found in $ZSH/plugins/
+# Custom plugins may be added to $ZSH_CUSTOM/plugins/
+plugins=(git) # Git plugin is usually enabled by default
+
+source $ZSH/oh-my-zsh.sh
+
+# User configuration
+# export MANPATH="/usr/local/man:$MANPATH"
+# export LANG=en_US.UTF-8
+
+# Preferred editor for local and remote sessions
+# if [[ -n $SSH_CONNECTION ]]; then
+#   export EDITOR='vim'
+# else
+#   export EDITOR='mvim'
+# fi
+
+# Compilation flags
+# export ARCHFLAGS="-arch x86_64"
+
+# Set personal aliases, overriding those provided by oh-my-zsh libs,
+# plugins, and themes. Aliases can be placed here, though oh-my-zsh
+# users are encouraged to define aliases within the ZSH_CUSTOM folder.
+# For a full list of active aliases, run `alias`.
+#
+# Example aliases
+# alias zshconfig="mate ~/.zshrc"
+# alias ohmyzsh="mate ~/.oh-my-zsh"
+alias vim='nvim'
+alias vi='nvim'
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,100 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  -- ToggleTerm (terminal manager)
+  {
+    "akinsho/toggleterm.nvim",
+    version = "*",
+    config = function()
+      require("toggleterm").setup({
+        direction = "float",  -- or "horizontal"/"vertical"
+        open_mapping = [[<leader>tt]],  -- Optional: Direct mapping here instead of mappings.lua
+      })
+    end,
+  },
+
+  -- Go.nvim (Go development)
+  {
+    "ray-x/go.nvim",
+    ft = "go",  -- Only load for Go files
+    config = function()
+      require("go").setup({
+        goimport = "gopls",  -- Use gopls for imports
+        gofmt = "gofmt",     -- Use gofmt
+        lsp_keymaps = false, -- Disable default LSP keymaps (use yours from mappings.lua)
+      })
+    end,
+    dependencies = {  -- Optional: Install these if you need debugging
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+  },
+
+  -- swenv.nvim (Python virtualenv switcher)
+  {
+    "AckslD/swenv.nvim",
+    ft = "python",  -- Only load for Python files
+    config = function()
+      require("swenv").setup({
+        venvs_path = vim.fn.expand("~/venvs"),  -- Path to your Python venvs
+      })
+    end,
+  },
+
+  -- nvim-dap-python (Python debugging)
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    config = function()
+      require("dap-python").setup("~/.virtualenvs/debugpy/bin/python")  -- Path to debugpy
+    end,
+  },
+
+  -- UI Theme (optional - replace with your preferred theme)
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = { user_default_options = { names = false } },  -- Better color highlighting
+  },
+  {
+    "devsjc/vim-jb",
+    lazy = false,
+    config = function()
+      vim.g.jb_plugin_option = "value"
+    end,
+  },
+  {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {},
+  -- stylua: ignore
+  keys = {
+    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+    { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+    { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+  },
+
+}
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
